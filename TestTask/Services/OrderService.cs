5f03394a160bbb213df4b6a3a2445611470a59c6
Ilja Garbuz <ilja.garbuz@ifortex.com>
2024-02-21 14:45:46 +0100

    initial

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public OrderService(ApplicationDbContext dbContext)
+        {
+                this.dbContext = dbContext;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            return await this.dbContext.Orders.FirstOrDefaultAsync( x => x.Price<50);
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await this.dbContext.Orders.ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext _context)
+        {
+                this._context = _context;
+        }
+        public async Task<User> GetUser()
+        {
+            return await this._context.Users.FirstOrDefaultAsync(x=> x.Status == Enums.UserStatus.Inactive);
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await this._context.Users.ToListAsync();
+        }
+    }
+}
